279765027b6590a85b1ec5563ffbf148fd193ef2
Marc-Philipp Esser <m-esser@mail.de>
2023-05-20 22:29:47 +0200

    Add table description and keys for MEDIA.meda table

--- src/utils/config.py
+++ src/utils/config.py
@@ -27,6 +27,10 @@ class InstagramRequestParams(BaseModel):
         "benedikt.hoehny",
     ]
 
+    # How many User should data for Media be requested for
+    media_data_user_limit: int = 1
+    media_data_chunk_size: int = 1
+
 
 class DataProcessingParams(BaseModel):
     is_initial_data_ingestion: bool = True
@@ -36,6 +40,7 @@ class DataProcessingParams(BaseModel):
 class SourceToTargetKeyMapping(BaseModel):
     """Raw Data Keys to Database Table columns mapping"""
 
+    # `USER.user` Table
     user_keys: list[str] = [
         "pk",
         "username",
@@ -47,6 +52,8 @@ class SourceToTargetKeyMapping(BaseModel):
         "category_name",
         "category",
     ]
+
+    # `USER.user_profile_setting` Table
     user_profile_setting_keys: list[str] = [
         "pk",
         "profile_pic_url",
@@ -57,6 +64,8 @@ class SourceToTargetKeyMapping(BaseModel):
         "biography",
         "external_url",
     ]
+
+    # `USER.user_contact_details` Table
     user_contact_details_keys: list[str] = [
         "pk",
         "public_email",
@@ -65,14 +74,36 @@ class SourceToTargetKeyMapping(BaseModel):
         "public_phone_number",
         "business_contact_method",
     ]
-    user_adresses_keys: list[str] = [
-        "pk",
-        "address_street",
-        "city_id",
-        "city_name",
-        "latitude",
-        "longitude",
-        "zip",
-        "instagram_location_id",
-    ]
+
+    # `USER.user_adress` Table
+    user_adress_keys: list[str] = (
+        [
+            "pk",
+            "address_street",
+            "city_id",
+            "city_name",
+            "latitude",
+            "longitude",
+            "zip",
+            "instagram_location_id",
+        ],
+    )
+
+    # `USER.account_type` Table
     # account_type_keys: list[str] = ["pk"]
+
+    # `MEDIA.media`
+    media_keys: list[str] = [
+        "pk",
+        "user_id",
+        "location",
+        "media_type",
+        "title",
+        "caption_text",
+        "thumbnail_url",
+        "product_type",
+        "video_url",
+        "video_duration",
+        "comments_disabled",
+        "taken_at",
+    ]
